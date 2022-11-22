da6caa33e8cc2875e8a5dcd2b17beb63da8396ff
varshaShiva <94578973+varshaShiva@users.noreply.github.com>
2022-11-21 20:55:24 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function nextPage(){
+    window.location="page_2.html";
+}
+//	<img style="width:2540px;height: 150px;"src="aboveHEADER.png">
+//<img style="width:2540px;height: 600px;"src="header.png"></img>
